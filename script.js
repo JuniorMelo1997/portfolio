b79da149ea9a7447e7e74d06cf23920c60c48d7e
Genival Melo Junior <genivaldemelojr@gmail.com>
2021-04-16 19:35:05 -0300

    :package: conhecimentos e projetos

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function changeSidebar(){
     sidebar.classList.toggle("sidebarNotActive");
     burger.classList.toggle("closeSidebar");
 }
+
+
+/* -------Projetos section--------- */
+
+function changeCardInfoVisibility(i){
+    document.querySelectorAll(".projetos .card .card-info")[i].classList.toggle("inactive");
+}
